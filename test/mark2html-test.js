d4ab88ee36793194059ef97c15969fa90d9ee334
saltfactory <saltfactory@gmail.com>
2014-07-16 11:17:07 +0900

    add helper.js and logger.js

--- test/mark2html-test.js
+++ test/mark2html-test.js
@@ -4,7 +4,7 @@
 var mark2html = require('../lib/mark2html');
 
 var options = {
-  src:'/Projects/Blogs/blog.saltfactory.net/_posts/2014-07-07-create-tistory-editor-package-in-atom-editor.md',
+  src:'/Users/saltfactory/Dropbox/Blog/posts/2014-07-07-create-tistory-editor-package-in-atom-editor.md',
   destDir:'/Users/saltfactory/Dropbox/Blog/output',
   imageCopy:true,
   datauri:true,
@@ -22,8 +22,3 @@ var options = {
   }
 }
 mark2html.convert(options);
-
-//mark2html.checkDest();
-
-
-//var argv = require('minimist');
